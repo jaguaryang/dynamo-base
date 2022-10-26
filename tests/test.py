e7d69ea62b52f831581cbd5133285c33d1be5f5c
Jack Yang <jack.yang@influxdata.nz>
2022-10-27 12:30:38 +1300

    testing

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
 from DynamoBase import DynamoBase
 
+DynamoBase.table_region = "ap-southeast-2"
 DynamoBase.table_name = "users"
 
 user = DynamoBase.get_first(query={"first_name": "Jackson"})
