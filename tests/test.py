9924d1b419513614e2ed3a007c39f3260a0014d6
Jack Yang <jack.yang@influxdata.nz>
2022-10-27 15:17:20 +1300

    testing

--- tests/test.py
+++ tests/test.py
@@ -8,5 +8,5 @@ DynamoBase.table_region = "ap-southeast-2"
 DynamoBase.table_name = "users"
 
 # DynamoBase.put_item(Item={"first_name": "Jackson", "age": 15})
-user = DynamoBase.get_first(query={"first_name": "Jackson"}, ScanIndexForward=False)
+user = DynamoBase.get_first(query={"first_name": "Jackson"}, ScanIndexForward=True)
 print(user)
